b5f79e884ae3e3ef304cb19d9d1a4c99e5446fbc
pigeons-apple <s4100787@student.rmit.edu.au>
2026-08-31 18:21:28 +1000

    feat: add STT client creation

--- backend/app/speech-to-text.py
+++ backend/app/speech-to-text.py
@@ -1,6 +1,8 @@
 import os
 
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from dotenv import load_dotenv
+from ibm_watson import SpeechToTextV1
 
 
 load_dotenv()
@@ -17,4 +19,14 @@ def get_environment_variable(variable_name):
     return value
 
 def create_stt_client():
-    
+    stt_api_key = get_environment_variable(
+        "SPEECH_TO_TEXT_API_KEY"
+    )
+    stt_service_url = get_environment_variable(
+        "SPEECH_TO_TEXT_URL"
+    )
+
+    client = SpeechToTextV1(authenticator=IAMAuthenticator(stt_api_key))
+    client.set_service_url(stt_service_url)
+
+    return client
